683065246c7b1be1a1744187fcee7597e9a81196
HoangLong <giabao5995@gmail.com>
2018-11-13 00:14:24 +0100

    try to upload file

--- src/app/components/file-selection/file-selection.component.ts
+++ src/app/components/file-selection/file-selection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {  FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
+import { Constants } from 'src/app/shared/constatns';
 
 @Component({
   selector: 'app-file-selection',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileSelectionComponent implements OnInit {
 
+  uploader: FileUploader = new FileUploader({url: Constants.ROOT_URL+'upload',itemAlias: 'csv'});
   constructor() { }
 
   ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+         console.log('ImageUpload:uploaded:', item, status, response);
+         alert('File uploaded successfully');
+     };
   }
 
 }
