800dd433d16318759574f06393b3d6d17f71e0c6
HoangLong <giabao5995@gmail.com>
2018-12-04 22:26:46 +0100

    update informations file

--- src/app/components/file-infos/file-infos.component.ts
+++ src/app/components/file-infos/file-infos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { UploadService } from 'src/app/services/upload.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Constants } from 'src/app/shared/constatns';
 
 @Component({
   selector: 'app-file-infos',
@@ -13,7 +15,8 @@ export class FileInfosComponent implements OnInit {
   keywordsList:any = [];
   constructor(
     private formBuilder: FormBuilder,
-    private uploadService: UploadService
+    private uploadService: UploadService,
+    private localStorageService: LocalStorageService
   ) { 
     this.informationForm = this.formBuilder.group({
       fileName: ['',Validators.required],
@@ -37,7 +40,9 @@ export class FileInfosComponent implements OnInit {
       console.log(this.informationForm.value);
       this.uploadService.sendInformationsFile(this.informationForm.value).then(data => {
         console.log(data);
-      })
+        this.localStorageService.saveJsonInSessionStorage(Constants.STORAGE_KEYS.UPLOAD_STEP_5, data);
+        this.localStorageService.saveValueInSessionStorage(Constants.STORAGE_KEYS.UPLOAD_STEP,6);
+      });
     }
   }
 }
--- src/app/components/upload-validation/upload-validation.component.ts
+++ src/app/components/upload-validation/upload-validation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UploadService } from 'src/app/services/upload.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Constants } from 'src/app/shared/constatns';
 
 @Component({
   selector: 'app-upload-validation',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-validation.component.css']
 })
 export class UploadValidationComponent implements OnInit {
-
-  constructor() { }
+  informationForm:any;
+  keyword:any;
+  keywordsList:any = [];
+  constructor(
+    private formBuilder: FormBuilder,
+    private uploadService: UploadService,
+    private localStorageService: LocalStorageService
+  ) { 
+  }
 
   ngOnInit() {
+    let data = this.localStorageService.getJsonInsessionStorage(Constants.STORAGE_KEYS.UPLOAD_STEP_5);
+    this.informationForm = this.formBuilder.group({
+      fileName: [data.fileName,Validators.required],
+      autor: [data.autor,Validators.required],
+      description: [data.description,Validators.required],
+      keyword: [data.keywords.toString(),Validators.required]
+    });
   }
 
 }
